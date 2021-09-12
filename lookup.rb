d64d6e4818eec4a1c31c55fe038c95f60d99d168
satyatazi82 <89475868+satyatazi82@users.noreply.github.com>
2021-09-12 11:06:17 +0530

    Update lookup.rb

--- lookup.rb
+++ lookup.rb
@@ -1,49 +1,53 @@
 def get_command_line_argument
-  # ARGV is an array that Ruby defines for us,
-  # which contains all the arguments we passed to it
-  # when invoking the script from the command line.
-  # https://docs.ruby-lang.org/en/2.4.0/ARGF.html
-  if ARGV.empty?
-    puts "Usage: ruby lookup.rb <domain>"
-    exit
-  end
-  ARGV.first
+	# ARGV is an array that Ruby defines for us,
+	# which contains all the arguments we passed to it
+	# when invoking the script from the command line.
+	# https://docs.ruby-lang.org/en/2.4.0/ARGF.html
+	if ARGV.empty?
+		puts "Usage: ruby lookup.rb <domain>"
+		exit
+	end
+	ARGV.first
 end
-
 # `domain` contains the domain name we have to look up.
 domain = get_command_line_argument
-
 # File.readlines reads a file and returns an
 # array of string, where each element is a line
 # https://www.rubydoc.info/stdlib/core/IO:readlines
-
 dns_raw = File.readlines("zone")
-# dns_raw.split(',')
-
-def parse_dns(string)
-  data = []
-  dns_raw.each do |line|
-    ar = line.split(',')
-  
-    data.push({
-      'type': ar[0],
-      'src': ar[1],
-      'dest': ar[2]
-    })
-    #do something with line
-  end
-  return data
+# ..
+#puts(dns_raw)
+def parse_dns(raw)
+	# raw.delete_at(0)
+	hash = {}
+	raw.each do |line|
+				if line[0] == '#'
+			next
+		end
+				if line.split(',').length() == 1
+			next
+		end
+				record = line.split(',')
+		hash[record[1].gsub(/\s+/, "")] = {
+			"type" => record[0].gsub(/\s+/, ""),
+			"src" => record[1].gsub(/\s+/, ""),
+			"dest" => record[2].gsub(/\s+/, "")
+		}
+	end
+	return hash
 end
-
-def resolve(rec, lookup, domain)
-  dns_records[:type]
+def resolve(dns_records, lookup, domain)
+	rec = dns_records[domain]
+	lookup.push(rec['dest'])
+	#puts(rec)
+	if rec["type"] == 'A'
+		return lookup
+	else
+		return resolve(dns_records, lookup, rec["dest"])
+	end
 end
 # ..
 # ..
-# FILL YOUR CODE HERE
-# ..
-# ..
-
 # To complete the assignment, implement `parse_dns` and `resolve`.
 # Remember to implement them above this line since in Ruby
 # you can invoke a function only after it is defined.
